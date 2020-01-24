6277ca1637d1e628d40e921b776ba6b9019f248a
Anish Adhikari <aanish@uber.com>
2020-01-23 17:35:39 -0800

    Added entitynotexisterror

--- thrift/history.thrift
+++ thrift/history.thrift
@@ -168,6 +168,11 @@ struct RespondActivityTaskCanceledRequest {
   20: optional shared.RespondActivityTaskCanceledRequest cancelRequest
 }
 
+struct RefreshWorkflowTasksRequest {
+  10: optional string domainUIID
+  20: optional shared.RefreshWorkflowTasksRequest request
+}
+
 struct RecordActivityTaskStartedRequest {
   10: optional string domainUUID
   20: optional shared.WorkflowExecution workflowExecution
@@ -892,5 +897,17 @@ service HistoryService {
       3: shared.ServiceBusyError serviceBusyError,
       4: shared.EntityNotExistsError entityNotExistError,
       5: ShardOwnershipLostError shardOwnershipLostError,
+    )
+
+  /**
+  * RefreshWorkflowTasks refreshes all tasks of a workflow
+  **/
+  void RefreshWorkflowTasks(1: RefreshWorkflowTasksRequest request)
+    throws (
+      1: shared.BadRequestError badRequestError,
+      2: shared.InternalServiceError internalServiceError,
+      3: ShardOwnershipLostError shardOwnershipLostError,
+      4: shared.ServiceBusyError serviceBusyError,
+      5: shared.EntityNotExistsError entityNotExistError,
     )
 }
