b8700886ba4557b2710d93a98b47183150723ef9
duoertai <duoertai@gmail.com>
2022-06-23 17:32:02 -0700

    update indexer message thrift definition (#110)

--- thrift/indexer.thrift
+++ thrift/indexer.thrift
@@ -28,6 +28,12 @@ enum MessageType {
   Create
 }
 
+enum VisibilityOperation {
+  RecordStarted
+  RecordClosed
+  UpsertSearchAttributes
+}
+
 enum FieldType {
   String
   Int
@@ -50,4 +56,5 @@ struct Message {
   40: optional string runID
   50: optional i64 (js.type = "Long") version
   60: optional map<string,Field> fields
+  70: optional VisibilityOperation visibilityOperation
 }
