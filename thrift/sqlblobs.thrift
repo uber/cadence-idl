c98dfcc9a76f6497265767c1586487dcc212a203
David Porter <david.porter@uber.com>
2023-04-24 17:29:58 -0700

    corrects thrift changes to include persistence layer

--- thrift/sqlblobs.thrift
+++ thrift/sqlblobs.thrift
@@ -69,6 +69,7 @@ struct DomainInfo {
   50: optional i64 (js.type = "Long") failoverEndTime
   52: optional i64 (js.type = "Long") previousFailoverVersion
   54: optional i64 (js.type = "Long") lastUpdatedTime
+  56: optional shared.IsolationGroupConfiguration isolationGroupConfiguration
 }
 
 struct HistoryTreeInfo {
