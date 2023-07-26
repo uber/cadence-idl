27b89f123daec49fa5edfc02741668480d54dd56
Ilya Ozherelyev <ilya.ozherelyev@uber.com>
2023-07-26 20:10:04 +0200

    add missing parentInitatedId also

--- thrift/shared.thrift
+++ thrift/shared.thrift
@@ -392,6 +392,7 @@ struct WorkflowExecutionInfo {
   60: optional i64 (js.type = "Long") historyLength
   70: optional string parentDomainId
   71: optional string parentDomainName
+  72: optional i64 parentInitatedId
   80: optional WorkflowExecution parentExecution
   90: optional i64 (js.type = "Long") executionTime
   100: optional Memo memo
